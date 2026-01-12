2680a1d9a342c0101b4ddbc0a1c1e8599301b9a6
Umeshpatil324 <uc2073593@gmail.com>
2026-01-13 00:10:46 +0530

    Add files via upload

---
+++ BasicArray/com/Printsarr.java
@@ -0,0 +1,14 @@
+package BasicArray.com;
+
+public class Printsarr {
+	public static void main(String[] args) {
+
+		// static methods of array
+
+		int[] marks = { 25, 48, 58, 78, 69, 48 };
+		for (int i = 0; i < marks.length; i++) {
+			System.out.print(" " + marks[i]);
+		}
+
+	}
+}
---
+++ BasicArray/com/SUMarr.java
@@ -0,0 +1,14 @@
+package BasicArray.com;
+
+public class SUMarr {
+	public static void main(String[] args) {
+		 int [] no = {25,78,95,3,6,88,7,55,88,65};
+		  int sum = 0;
+		 for(int i=0; i<no.length; i++) {
+			 sum = sum + no[i];
+			 
+		 }
+		 System.out.println("total no is : " + sum);
+	}
+
+}
