d53c157baea6675cc1534b90d10134e6ef2d7acf
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-01-20 22:50:00 +0530

    Add files via upload

---
+++ datatypes/DataTypes.java
@@ -0,0 +1,19 @@
+package com.datatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		System.out.println("short Range =  " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+		System.out.println("byte Range =  " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+		System.out.println("int Range =  " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+		System.out.println("long Range =  " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+		System.out.println("float Range =  " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+		System.out.println("double Range =  " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+		System.out.println("char Range =  " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+		System.out.println("Boolean Range is not Fixed...!");
+
+	} {
+
+}
+}
