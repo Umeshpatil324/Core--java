0f7013fb84d8e18622d0ea9ac5a2507ee08567d2
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-02-03 23:15:43 +0530

    Add files via upload

---
+++ IfStatement/EvenNumber.java
@@ -0,0 +1,5 @@
+package IfStatement;
+
+public class EvenNumber {
+
+}
---
+++ IfStatement/PositiveNumber.java
@@ -0,0 +1,12 @@
+package IfStatement;
+
+public class PositiveNumber {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("Number is positive");
+        }
+    }
+}
+
